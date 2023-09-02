3cd6e0fcd79bd07127cedf2166118153fcdccb66
Denys Kovtun <denys.kovtun@gmail.com>
2023-09-02 15:02:30 +0300

    fix #3

--- contacts.js
+++ contacts.js
@@ -12,8 +12,8 @@ async function listContacts() {
 
 async function getContactById(contactId) {
   const contacts = await listContacts();
-  const res = contacts.find(i => i.id === contactId);
-  return res || null;
+  const response = contacts.find(i => i.id === contactId);
+  return response || null;
 }
 
 async function removeContact(contactId) {
@@ -22,9 +22,9 @@ async function removeContact(contactId) {
   if (index === -1) {
     return null;
   }
-  const [res] = contacts.splice(index, 1);
+  const [responce] = contacts.splice(index, 1);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return res;
+  return responce;
 }
 
 async function addContact(data) {
